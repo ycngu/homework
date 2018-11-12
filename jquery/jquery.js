d713141739bcab84d314d29af70966315a933fd2
ycngu <snl123698745@163.com>
2018-11-12 23:22:21 +0800

    fix bug

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -70,35 +70,51 @@ Node.prototype.toggleClass = function (classes) {
     }
 }
 
-window.Node2 = function (nodeOrSeletor) {
-    let node
+window.jQuery = function (nodeOrSeletor) {
+    let nodes = {}
     if (typeof nodeOrSeletor === 'string') {
-        node = document.querySelectorAll(nodeOrSeletor)
-    } else {
-        node = nodeOrSeletor
-    }
-
-    return {
-        getSiblings: function () {
-            var allChildren = node.parentNode.children
-
-            var array = {
-                length: 0
-            }
-
-            for (let i = 0; i < allChildren.length; i++) {
-                if (allChildren[i] !== node) {
-                    array[array.length] = allChildren[i];
-                    array.length += 1
-                }
-            }
-            return array
-        },
-        toogleClass: function () {
-            for (let key in classes) {
-                var methodName = classes[key] ? 'add' : 'remove'
-                node.classList[methodName](key)
-            }
+        let temp = document.querySelectorAll(nodeOrSeletor)
+        for (let i = 0; i < temp.length; i++) {
+            nodes[i] = temp[i]
+        }
+        nodes.length = temp.length
+    } else if (nodeOrSeletor instanceof Node) {
+        nodes = {
+            0: nodeOrSeletor,
+            length: 1
         }
     }
+
+
+    nodes.addClass = function (classes) {
+        //当classes不是一个数组时，会出错
+        if (typeof classes === 'string'){
+            for (let i = 0; i < nodes.length; i++) {
+                nodes[i].classList.add(classes)
+            }
+        } else {
+            classes.forEach(value => {
+                for (let i = 0; i < nodes.length; i++) {
+                    nodes[i].classList.add(value)
+                }
+            })
+        }
+    } 
+    nodes.getText = function () {
+        var texts = []
+        for (let i = 0; i < nodes.length; i++) {
+            texts.push(nodes[i].textContent)
+        }
+        return texts
+    }
+    nodes.setText = function (text) {
+        for (let i = 0; i < nodes.length; i++) {
+            nodes[i].textContent = text
+        }
+    }
+
+    return nodes
 }
+
+
+window.$ = jQuery
